e3e6df71a9c4b9ceb3982c57d51079ea73ac3be7
bernardphua <46226572+bernardphua@users.noreply.github.com>
2018-12-29 01:32:56 +0800

    initial upload

---
+++ controllers.js
@@ -0,0 +1,31 @@
+//CONTROLLERS   356225a3c4a96dbd2457265a0530015e  1352a59ccac755017f3abce51cca86f9
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function ($scope, $location, cityService) {
+
+    $scope.city = cityService.city;
+
+    $scope.$watch('city', function () {
+        cityService.city = $scope.city;
+    });
+
+    $scope.submit = function () {
+        $location.path("/forecast");
+    }
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService',
+    function ($scope, $routeParams, cityService, weatherService) {
+
+        $scope.city = cityService.city;
+        $scope.days = $routeParams.days || 2;
+
+        $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
+
+        $scope.convertToCelsius = function (degK) {
+            return Math.round(degK - 273.15);
+        }
+
+        $scope.convertToDate = function (dt) {
+            return new Date(dt * 1000);
+        }
+}]);
---
+++ services.js
@@ -0,0 +1,26 @@
+//SERVICES
+weatherApp.service('cityService', function () {
+    this.city = "New York, US";
+
+});
+
+weatherApp.service('weatherService', ['$resource', function ($resource) {
+
+    this.GetWeather = function (city, days) {
+
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", {
+            callback: "JSON_CALLBACK"
+        }, {
+            get: {
+                method: "JSONP"
+            }
+        });
+
+        return weatherAPI.get({
+            appid: "1352a59ccac755017f3abce51cca86f9",
+            q: city,
+            cnt: days
+        });
+    };
+
+}]);
